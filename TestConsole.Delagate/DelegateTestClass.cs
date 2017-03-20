40093f82d2167bb799ed09176d2d5a3fbd10cbf8
subhendus <suvendu.swain@gmail.com>
2017-03-20 22:46:51 +1000

    Delegates

---
+++ TestConsole.Delagate/DelegateTestClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole.Delagate
+{
+    class DelegateTestClass
+    {
+        public delegate void Del(string message);
+
+      
+
+       // public DelegateTestClass(Del handler)
+        public DelegateTestClass()
+        {
+           // handler("Hi Dude... This is Delegate");
+          ///  handler("Under Standing Delegates....");
+
+        }
+
+        public void DoCallBack(Del handler)
+        {
+            handler("Hi Dude... This is Delegate");
+        }
+    }
+}
---
+++ TestConsole.Delagate/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole.Delagate
+{
+    class Program
+    {
+
+        public static void DoSomeThing(string message)
+        {
+            Console.WriteLine(message);
+        }
+        static void Main(string[] args)
+        {
+            // DelegateTestClass dClass = new DelegateTestClass(x => { Console.WriteLine(x); });
+            //dClass.handler = (x) => { Console.WriteLine("Hi...."); };
+            //DelegateTestClass dClass = new DelegateTestClass(DoSomeThing);
+
+            DelegateTestClass dClass = new DelegateTestClass();
+            dClass.DoCallBack(DoSomeThing);
+
+            Console.ReadLine();
+        }
+    }
+}
